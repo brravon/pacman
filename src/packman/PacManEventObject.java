b1c8cd358b274c89aa3e75b0fc05a4c97fdb4f12
brravon <brravon@gmail.com>
2013-12-03 16:58:17 +0100

    test de commit

---
+++ src/packman/PacManEventObject.java
@@ -0,0 +1,10 @@
+package packman;
+
+
+public class PacManEventObject extends java.util.EventObject {
+	
+	private static final long serialVersionUID = 1L;
+	public PacManEventObject(Object o) {
+		super(o);
+	}
+}
